7729505bd5b326ceef9e43062951bb85c981f0be
timardex <timardex@gmail.com>
2021-07-09 15:18:21 +0200

    disbale btn

--- src/components/CreateEditRecipe/Recipe.js
+++ src/components/CreateEditRecipe/Recipe.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Recipe = (props) => {
   const { recipe, handleClick } = props;
+  const disableBtn = () => {
+    return recipe.ingredient !== '';
+  }
   return (
     <div>
       {recipe.name && <p>Name: {recipe.name}</p>}
@@ -11,8 +14,8 @@ const Recipe = (props) => {
         {recipe.ingredients.map((item, index) => {
           return <li key={index}>
                   {item}
-                  <button onClick={e => handleClick('delete', item)}>Delete</button>
-                  <button onClick={e => handleClick('edit', item)}>Edit</button>
+                  <button onClick={e => disableBtn() ? null : handleClick('delete', item)} disabled={disableBtn()}>Delete</button>
+                  <button onClick={e => disableBtn() ? null : handleClick('edit', item)} disabled={disableBtn()}>Edit</button>
                 </li>
         })}
       </ul>
